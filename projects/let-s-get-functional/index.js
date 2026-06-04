6239d215eb60f52e2b025ed28e41542e70bb98c2
antheasmith07-png <anthea.smith07@gmail.com>
2026-06-04 16:00:57 -0500

    as incomplete 6.4.26

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -1,14 +1,67 @@
 // #!/usr/bin/env node
 
 'use strict';
-
-var maleCount;
-
-var femaleCount;
-
-var oldestCustomer;
-  
-var youngestCustomer;
+//I: take in array of objects of male customers
+//O: return a count of male customers
+//C: n/a
+//E: n/a
+var maleCount = function(array) {    
+    var males = _.filter(array, function(customer){
+        return customer.gender === 'male';
+      });
+    
+      return males.length;
+    }       
+//I: creata a function that take in array of objects of female customers
+//O: return a count of female customers
+//C: n/a
+//E: n/a
+var femaleCount = function(array) {
+    let females = _.filter(array, function(customer){
+        return customer.gender === 'female';
+    });
+        return females.length;
+}
+//I: create function that takes in an array
+//O: return a string with the oldest customer name
+//C: n/a
+//E: n/a
+var oldestCustomer = function(array) {
+    //declare an accumulator value
+    var oldestCust = 0;
+    //use reduce function to find customers ages
+    let oldest = _.reduce(array, function(oldestCust, customer){
+        //if current customer is older than oldest cust return customer
+            if (customer.age > oldestCust.age) {
+            return customer;
+            } else {
+            //if not older than oldest cust return Oldest Cust
+            return oldestCust;
+    }
+});
+    // return oldest person's name
+            return oldest.name;
+}
+//I: create function that takes in an array
+//O: return a string with the oldest customer name
+//C: n/a
+//E: n/a  
+var youngestCustomer =  function(array){
+    //declare an accumulator value
+    var youngestCust = 0;
+    //use reduce function to find customers ages
+    let youngest = _.reduce(array, function(youngestCust, customer){
+        //if current customer is older than oldest cust return customer
+            if (customer.age < youngestCust.age) {
+            return customer;
+            } else {
+            //if not younger than youngest cust return Youngest Cust
+            return youngestCust;
+    }
+});
+    // return youngest person's name
+            return youngest.name;
+}
 
 var averageBalance;
 
